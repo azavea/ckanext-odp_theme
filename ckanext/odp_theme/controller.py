41f7503a25dc974d31d386f7941183f4699b6c03
Sharp Hall <shall@azavea.com>
2014-12-29 12:50:16 -0500

    Delete feedback when empty and store modification time
    Add line break for readability

--- ckanext/odp_theme/controller.py
+++ ckanext/odp_theme/controller.py
@@ -1,4 +1,6 @@
 
+import datetime
+
 import ckan.plugins as p
 import ckan.model as model
 from ckan.plugins import toolkit as tk
@@ -29,25 +31,29 @@ class UnpublishedFeedbackController(p.toolkit.BaseController):
                     c.user_feedback = UnpublishedFeedback()
                     c.user_feedback.dataset = c.pkg.id
                     c.user_feedback.user = c.userobj.id
-            # TODO insert/update
-            if tk.request.method == 'POST':
+            if tk.request.method == 'POST':  # INSERT/UPDATE/DELETE
                 data = tk.request.POST
                 comment_fields = [('economic', 'economic_comment'),
                                   ('social', 'social_comment'),
                                   ('public-service', 'public_service_comment'),
                                   ('other', 'other_comment')]
+                empty = True
                 for form_name, db_name in comment_fields:
                     checkbox = 'checkbox-{}'.format(form_name)
                     if checkbox in data and data[checkbox]:
+                        empty = False
                         if form_name not in data:
                             data[form_name] = ''
                         setattr(c.user_feedback, db_name, data[form_name])
                     else:
                         setattr(c.user_feedback, db_name, None)
-
-                c.user_feedback.save()
                 session = context['session']
-                session.add(c.user_feedback)
+                if not empty:
+                    c.user_feedback.modified = datetime.datetime.utcnow()
+                    c.user_feedback.save()
+                    session.add(c.user_feedback)
+                elif c.user_feedback.id:
+                    c.user_feedback.delete()
                 session.commit()
 
             c.pkg_feedback = UnpublishedFeedback.get_for_package(c.pkg.id).all()
@@ -55,4 +61,5 @@ class UnpublishedFeedbackController(p.toolkit.BaseController):
             tk.abort(401, tk._('Unauthorized to read package'))
         except tk.ObjectNotFound:
             tk.abort(404, tk._('Dataset not found'))
+
         return tk.render('feedback/feedback.html')
